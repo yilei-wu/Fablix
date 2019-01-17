31685f1d5e2387c3ba67f533179ac352b01e8e07
Brian <bjjx1999@live.com>
2019-01-17 15:46:51 -0800

    improve movie list, single movie layout

--- src/main/webapp/movie_list.js
+++ src/main/webapp/movie_list.js
@@ -1,12 +1,12 @@
 function getTableRow(data, row_num) {
     var row = $('<tr></tr>');
     row.append($('<td>' + (row_num + 1) + '</td>'))
-        .append($('<td>name...</td>'))
-        .append($('<td>year...</td>'))
-        .append($('<td>director...</td>'))
-        .append($('<td>genres...</td>'))
+        .append($('<td>' + data[row_num]['title'] + '</td>'))
+        .append($('<td>' + data[row_num]['year'] + '</td>'))
+        .append($('<td>' + data[row_num]['director'] + '</td>'))
+        .append($('<td>' + data[row_num]['genre_list'] + '</td>'))
         .append($('<td>stars...</td>'))
-        .append($('<td>rating...</td>'));
+        .append($('<td>' + data[row_num]['rating'] + '</td>'));
     return row;
 }
 
@@ -15,27 +15,28 @@ function getTableRow(data, row_num) {
 */
 function handleMovieListResult(resultData){
     $(function () {
+        console.log(resultData);
         var table_body = $('#movie-list > tbody');
-        for (var i = 0; i < 20; i++){
+        for (var i = 0; i < resultData.length; i++){
             // var row = $('<tr><td>' + i + '</td></tr>');
-            table_body.append(getTableRow(null, i))
+            table_body.append(getTableRow(resultData, i))
 
 
         }
     })
 }
 
-handleMovieListResult(null);
+// handleMovieListResult(null);
 
 // var table_body = $('#movie-list');
 // console.log(table_body);
 // var row = $('<tr>10000</tr>');
 // table_body.append(row);
 
-// $.ajax({
-//     dataType: "json",
-//     method: "GET",
-//     url: "api/stars",
-//     success: handleMovieListResult
-//     // error:
-// });
+$.ajax({
+    dataType: "json",
+    method: "GET",
+    url: "api/movie_list",
+    success: handleMovieListResult
+    // error:
+});
