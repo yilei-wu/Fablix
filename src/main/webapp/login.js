0ebc2aeec71034d0ec1fbc4adb2f8d26c4b37336
Brian <bjjx1999@live.com>
2019-01-25 10:27:05 -0800

    add login page, extract css

---
+++ src/main/webapp/login.js
@@ -0,0 +1,48 @@
+/**
+ * Handle the data returned by LoginServlet
+ * @param resultDataString jsonObject
+ */
+function handleLoginResult(resultDataString) {
+    var resultDataJson = JSON.parse(resultDataString);
+
+    console.log("handle login response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["status"]);
+
+    // If login succeeds, it will redirect the user to movie_list.html
+    if (resultDataJson["status"] === "success") {
+        window.location.replace("index.html");
+    } else {
+        // If login fails, the web page will display
+        // error messages on <div> with id "login_error_message"
+        console.log("show error message");
+        console.log(resultDataJson["message"]);
+        $("#login_error_message").text(resultDataJson["message"]);
+    }
+}
+
+/**
+ * Submit the form content with POST method
+ * @param formSubmitEvent
+ */
+function submitLoginForm(formSubmitEvent) {
+    console.log("submit login form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    formSubmitEvent.preventDefault();
+
+    $.post(
+        "api/login",
+        // Serialize the login form to the data sent by POST request
+        $("#login_form").serialize(),
+        (resultDataString) => handleLoginResult(resultDataString)
+    ).fail($("#login_error_message").text('Failed connect to api.'));
+}
+
+$(function () {
+    // Bind the submit action of the form to a handler function
+    $("#login_form").submit(submitLoginForm);
+});
