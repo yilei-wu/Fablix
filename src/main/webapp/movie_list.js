06ef6309f7d811ef1adb337cdc5e4b6ba8eaa50b
Brian <bjjx1999@live.com>
2019-02-02 22:27:38 -0800

    update sorting

--- src/main/webapp/movie_list.js
+++ src/main/webapp/movie_list.js
@@ -1,4 +1,5 @@
 var page_num = 1;
+var title, year, director, star, sort;
 
 function getTableRow(data, row_num) {
     var stars = getStars(data, row_num);
@@ -10,8 +11,88 @@ function getTableRow(data, row_num) {
         .append($('<td>' + data[row_num]['genre_list'] + '</td>'))
         .append($('<td></td>').append(stars))
         .append($('<td>' + data[row_num]['rating'] + '</td>'))
-        .append($('<td><button class="btn btn-primary" onclick=""><i class="fas fa-cart-plus"></i></button></td>'));
+        .append($('<td><button class="btn btn-primary"><i class="fas fa-cart-plus"></i></button></td>'));
+    row.find('button').attr('onclick', 'addMovieToCart("m_' + data[row_num ]['movie_id'] + '");alert("succeed")');
     return row;
+}
+
+function updateInfo() {
+
+
+
+    $('#page_container').pagination({
+        dataSource: 'api/search_movie?title=' + title + '&year=' + year + '&director=' + director +
+            '&star=' + star + '&sort=' + sort,
+        // locator: function () {
+        //     return 'movielist'
+        // },
+        locator: 'movielist',
+        totalNumberLocator: function (response) {
+            // console.log(response['total_page']);
+            return response['total_number'];
+            // return 2000
+        },
+        showGoInput: true,
+        showGoButton: true,
+        pageSize: 22,
+        // pageNumber: page_num,
+        className: 'paginationjs-theme-blue',
+        afterRender: function (){
+            // console.log(this);
+            // sessionStorage.setItem('page', $('#page_container').pagination('getSelectedPageNum'));
+            // changePage()
+        },
+        // ajax: {
+        //     beforeSend: function() {
+        //         dataContainer.html('Loading data from flickr.com ...');
+        //     }
+        // },
+        callback: function(data, pagination) {
+            // template method of yourself
+            handleMovieListResult(data, pagination)
+        },
+        alias: {
+            pageNumber: 'page',
+            pageSize: 'records'
+        }
+    });
+}
+
+function resort(key) {
+    let currentSort = sessionStorage.getItem('sort');
+    if (key === 't') {
+        if (currentSort === 'ta') {
+            sessionStorage.setItem('sort', 'td');
+            sort = 'td';
+        } else {
+            sessionStorage.setItem('sort', 'ta');
+            sort = 'ta';
+        }
+    } else if (key === 'r') {
+        if (currentSort === 'ra') {
+            sessionStorage.setItem('sort', 'rd');
+            sort = 'rd';
+        } else {
+            sessionStorage.setItem('sort', 'ra');
+            sort = 'ra';
+        }
+    }
+
+    updateInfo();
+    // if (currentSort === 'ta') {
+    //     sessionStorage.setItem('sort', 'td');
+    // } else if (currentSort === 'td') {
+    //     sessionStorage.setItem('sort', 'ta');
+    // } else if (currentSort === 'ra') {
+    //     sessionStorage.setItem('sort', 'rd');
+    // } else if (currentSort === 'rd') {
+    //     sessionStorage.setItem('sort', 'ra');
+    // }
+
+    // $('#page_container').pagination({
+    //     dataSource: 'api/search_movie?title=' + title + '&year=' + year + '&director=' + director +
+    //         '&star=' + star + '&sort=' + sort,
+    // })
 }
 
 function changePage() {
@@ -70,9 +151,8 @@ $(function () {
     },
         300);
 
-    var title, year, director, star, sort;
-    // var page_num;
 
+    // var page_num;
     let from = getParameterByName('from');
     if (from === 'search') {
         title = getParameterByName('title');
@@ -108,57 +188,11 @@ $(function () {
     }
     console.log(page_num);
 
-
-
-
-    $('#page_container').pagination({
-        dataSource: 'api/search_movie?title=' + title + '&year=' + year + '&director=' + director +
-                '&star=' + star + '&sort=' + sort,
-        // locator: function () {
-        //     return 'movielist'
-        // },
-        locator: 'movielist',
-        totalNumberLocator: function (response) {
-            // console.log(response['total_page']);
-            return response['total_number'];
-            // return 2000
-        },
-        showGoInput: true,
-        showGoButton: true,
-        pageSize: 22,
-        pageNumber: page_num,
-        className: 'paginationjs-theme-blue',
-        afterRender: function (){
-            // console.log(this);
-            // sessionStorage.setItem('page', $('#page_container').pagination('getSelectedPageNum'));
-            // changePage()
-        },
-        // ajax: {
-        //     beforeSend: function() {
-        //         dataContainer.html('Loading data from flickr.com ...');
-        //     }
-        // },
-        callback: function(data, pagination) {
-            // template method of yourself
-            handleMovieListResult(data, pagination)
-        },
-        alias: {
-            pageNumber: 'page',
-            pageSize: 'records'
-        }
-    });
+    updateInfo();
 
 
 
 
 
-    // $.ajax({
-    //     dataType: "json",
-    //     method: "GET",
-    //     url: 'api/search_movie?title=' + title + '&year=' + year + '&director=' + director +
-    //     '&star=' + star + '&page=' + page + '&sort=' + sort + '&records=' + records,
-    //     success: handleMovieListResult
-    //     // error:
-    // });
 });
 
