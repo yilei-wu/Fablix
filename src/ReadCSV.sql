62c6a3d2a966ffdcf9a43f5f3243c90c41415cad
Brian <bjjx1999@live.com>
2019-02-18 21:48:09 -0800

    update csv, add report, project 3 done

--- src/ReadCSV.sql
+++ src/ReadCSV.sql
@@ -1,26 +1,58 @@
 USE 122B;
 
-# LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/src/test.csv'
-#   IGNORE INTO TABLE movies
-#   FIELDS TERMINATED BY ','
-#   ENCLOSED BY '"'
-#   ESCAPED BY '"'
-#   LINES TERMINATED BY '\n'
-#   IGNORE 1 LINES
-#   (id, title, year, director);
+ALTER TABLE `122B`.movies ADD UNIQUE movie_info(title, year, director);
+ALTER TABLE `122B`.genres ADD UNIQUE genre_name(name);
 
-LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/cast.csv'
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/movies.csv'
   IGNORE INTO TABLE `122B`.movies
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
-  ESCAPED BY '"'
+  ESCAPED BY '\\'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
-  (id, title, director);
+  (director, id, title, year);
 
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/genre.csv'
+  IGNORE INTO TABLE `122B`.genres
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (name, id);
 
-# SELECT * FROM `122B`.genres
-# INTO OUTFILE 'asdf/temp.csv'
-#   FIELDS TERMINATED BY ','
-#   ENCLOSED BY '"'
-#   LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/genre_in_movie.csv'
+  IGNORE INTO TABLE `122B`.genres_in_movies
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (genreId, movieId);
+
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/star.csv'
+  IGNORE INTO TABLE `122B`.stars
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (id, name);
+
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/star_in_movie.csv'
+  IGNORE INTO TABLE `122B`.stars_in_movies
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (movieId, starId);
+
+LOAD DATA LOCAL INFILE '/Users/BrianJiang/JavaProjects/Fablix/actors.csv'
+  IGNORE INTO TABLE `122B`.stars
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  ESCAPED BY '\\'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (id, name, birthYear);
