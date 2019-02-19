1680c884eacd31ee0c83e8b472fa0c0f459b204b
Brian <bjjx1999@live.com>
2019-02-18 17:17:19 -0800

    update id generator

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,12 +1,17 @@
 import java.util.Random;
+import java.util.UUID;
 
 public class Generator {
     public static String GetID(int length) {
-        String result = "";
-        for (int i = 0; i < length ; ++i) {
-            Random category = new Random();
-        }
+//        String result = "";
+//        for (int i = 0; i < length ; ++i) {
+//            Random category = new Random();
+//        }
+//        return result;
+        return UUID.randomUUID().toString().substring(0, length);
+    }
 
-        return result;
+    public static void main(String[] args){
+        System.out.println(GetID(10));
     }
 }
