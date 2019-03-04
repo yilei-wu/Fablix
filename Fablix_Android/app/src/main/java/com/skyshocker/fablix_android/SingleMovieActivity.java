cd1bf09de4e9ac5f6538f4396ea42eacceddb5f8
pc_win_Elay <yileiw5@uci.edu>
2019-03-04 11:07:43 -0800

    Merge branch 'master' of C:\Users\pc_win_Elay\Desktop\cs122B.1 with conflicts.

---
+++ Fablix_Android/app/src/main/java/com/skyshocker/fablix_android/SingleMovieActivity.java
@@ -0,0 +1,30 @@
+package com.skyshocker.fablix_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SingleMovieActivity extends AppCompatActivity {
+
+    //ui reference
+    private EditText title;
+    private EditText year;
+    private EditText director;
+    private EditText stars;
+    private EditText genres;
+    private Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_movie);
+
+        title = findViewById(R.id.Title);
+        year = findViewById(R.id.Year);
+        director = findViewById(R.id.Director);
+        stars = findViewById(R.id.Stars);
+        genres = findViewById(R.id.Genres);
+
+        back = findViewById(R.id. ba)
+    }
+}
