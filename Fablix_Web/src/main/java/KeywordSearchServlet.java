2a65063c55f5393a3c7512ec7309e05042d50189
pc_win_Elay <yileiw5@uci.edu>
2019-03-18 23:05:27 -0700

    master scaled 1

--- Fablix_Web/src/main/java/KeywordSearchServlet.java
+++ Fablix_Web/src/main/java/KeywordSearchServlet.java
@@ -94,7 +94,7 @@ public class  KeywordSearchServlet extends HttpServlet
             String select_query  = "SELECT  movies.id, title, `year`, director, rating, GROUP_CONCAT(distinct genres.name SEPARATOR ', ') as gname, GROUP_CONCAT(distinct  stars.name SEPARATOR ',') as sname";
             String from_query = " FROM movies left join ratings r on movies.id = r.movieId, genres, genres_in_movies, stars, stars_in_movies";
             String where_join = " WHERE movies.id = genres_in_movies.movieId and genres_in_movies.genreId = genres.id and stars_in_movies.movieId = movies.id and stars_in_movies.starId = stars.id";
-            String title_condition = " AND MATCH(movies.title) AGAINST(\'" + f + "\' IN BOOLEAN MODE) " ;
+            String title_condition = " AND MATCH(movies.title) AGAINST( ? IN BOOLEAN MODE) " ;
             String group_clause = " GROUP BY id";
             String order_clause = get_sort_clause(sort);
             String offset_clause = get_offset_clause(page, records);
@@ -103,21 +103,21 @@ public class  KeywordSearchServlet extends HttpServlet
             String queryt = "Select count(distinct movies.id) as a " + from_query + where_join + title_condition;
 //            String fuzzy_query = "SELECT * FROM [" + query + "] WHERE ed(title,?) <= 3";
 //            String fuzzy_count = "SELECT COUNT(*) AS a FROM " + fuzzy_query;
-//            PreparedStatement statement = dbcon.prepareStatement(query);
-//            PreparedStatement statement1 = dbcon.prepareStatement(queryt);
+            PreparedStatement statement = dbcon.prepareStatement(query);
+            PreparedStatement statement1 = dbcon.prepareStatement(queryt);
 
-            Statement statement2 = dbcon.createStatement();
-            Statement statement3 = dbcon.createStatement();
-
-            ResultSet resultSet = statement2.executeQuery(query);
-            ResultSet w  = statement3.executeQuery(queryt);
-            //statement.setString(1, f );
-            //statement.setString(2, keyword);
-            //statement1.setString(1 , f );
-            //statement1.setString(2, keyword);
+//            Statement statement2 = dbcon.createStatement();
+//            Statement statement3 = dbcon.createStatement();
+//
+//            ResultSet resultSet = statement2.executeQuery(query);
+//            ResultSet w  = statement3.executeQuery(queryt);
+            statement.setString(1, f );
+//            statement.setString(2, keyword);
+            statement1.setString(1 , f );
+//            statement1.setString(2, keyword);
             long tj_start = System.nanoTime();
-//            ResultSet resultSet = statement.executeQuery();
-//            ResultSet w = statement1.executeQuery();
+            ResultSet resultSet = statement.executeQuery();
+            ResultSet w = statement1.executeQuery();
             long tj1 = System.nanoTime();
             tj += (tj1 - tj_start);
             //System.out.println(query);
@@ -148,12 +148,12 @@ public class  KeywordSearchServlet extends HttpServlet
                 JsonArray json_stars = new JsonArray();
                 String star_query = "SELECT distinct stars.name, stars.id\n" +
                         "FROM movies, stars, stars_in_movies\n" +
-                        "WHERE movies.id = \'" + sid + "\' and movies.id = stars_in_movies.movieId and stars_in_movies.starId = stars.id;";
-//                PreparedStatement s = dbcon.prepareStatement(star_query);
-//                s.setString(1, sid);
-                Statement statement = dbcon.createStatement();
+                        "WHERE movies.id = ? and movies.id = stars_in_movies.movieId and stars_in_movies.starId = stars.id;";
+                PreparedStatement s = dbcon.prepareStatement(star_query);
+                s.setString(1, sid);
+//                Statement statement = dbcon.createStatement();
                 tj2 = System.nanoTime();
-                ResultSet r = statement.executeQuery(star_query);
+                ResultSet r = s.executeQuery();
                 tj_end = System.nanoTime();
                 tj += (tj_end- tj2);
                 while (r.next())
